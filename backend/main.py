cf42ea82fe22e8cfaacc57dd92699c98a386e21f
Mihir Phatak <32159532+mihirphatak@users.noreply.github.com>
2026-03-09 21:10:08 -0500

    Added CORS

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from database import engine, Base
 from api.routes_projects import router as project_router
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+    "https://dev-diary-platform.vercel.app/"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 Base.metadata.create_all(bind=engine)
 
